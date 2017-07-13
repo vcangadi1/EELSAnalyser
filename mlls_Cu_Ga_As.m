6286e2f60721612663a98c75c16342278b828ee2
Veerendra C Angadi <vcangadi1@users.noreply.github.com>
2017-07-13 17:42:47 +0100

    mlls fit for SI of Multilayer solar cell 950eV offset SI -- background subtracted and modelled with diff cross-section

--- mlls_Cu_Ga_As.m
+++ mlls_Cu_Ga_As.m
@@ -5,23 +5,51 @@ clear all
 
 EELS = load('C:\Users\elp13va.VIE\Desktop\EELS data\Ge-basedSolarCell_24082015\artifact_removed_EELS Spectrum Image disp1offset950time2s.mat');
 EELS = EELS.EELS;
+l = EELS.energy_loss_axis';
+
 EELZ = load('C:\Users\elp13va.VIE\Desktop\EELS data\Ge-basedSolarCell_24082015\artifact_removed_EELS Spectrum Image disp0.2offset0time0.1s.mat');
 EELZ = EELZ.EELS;
-l = EELS.energy_loss_axis';
 
 %% Load differential cross sections
 
-dfcCu = diffCS_L23(29,923,197,16.6,l);
-dfcGa = diffCS_L23(31,1115,197,16.6,l);
-dfcAs = diffCS_L23(33,1323,197,16.6,l);
+dfcCu = diffCS_L23(29,928,197,16.6,l);
+dfcGa = diffCS_L23(31,1120,197,16.6,l);
+dfcAs = diffCS_L23(33,1325,197,16.6,l);
+
+%dc = [dfcCu, dfcGa, dfcAs];
+
+%% define lb, ub, p0 for lsqcurvefit
+lb = [0,0,0];
+ub = [1E10,1E10,1E10];
+
+p0 = [300,300,300];
 
 %% fit background
 
-for ii = EELS.SI_x:-1:1
-    for jj = EELS.SI_y:-1:1
+for ii = 90:-1:1
+    for jj = 43:-1:1
+        fprintf('Fitting (%d,%d)\n',ii,jj);
         S = squeeze(EELS.SImage(ii,jj,:));
+        % smooth spectrum with spline function
         S = feval(Spline(l,S),l);
-        B(ii,jj,1:EELS.SI_z) = feval(Exponential_fit(l(1:63),S(1:63)),l);
+        Sm(ii,jj,1:EELS.SI_z) = S;
+        % correct dispersion
+        Z = resample(squeeze(EELZ.SImage(ii,jj,:)),1,5);        
+        % plural scatter differential cross-section
+        pdfcCu = plural_scattering(dfcCu, Z);
+        pdfcGa = plural_scattering(dfcGa, Z);
+        pdfcAs = plural_scattering(dfcAs, Z);
+        % model background with exponential function
+        B(ii,jj,1:EELS.SI_z) = S - feval(Exponential_fit(l(1:63),S(1:63)),l);
+        rS = squeeze(B(ii,jj,:));
+        fun = @(p,l) p(1)*pdfcCu + (p(2)+p(1))*pdfcGa + (p(3)+p(2)+p(1))*pdfcAs;
+        p = lsqcurvefit(fun,p0,l,rS,lb,ub);
+        %A(ii,jj) = p(1);
+        %r(ii,jj) = p(2);
+        Cu(ii,jj) = p(1);
+        Ga(ii,jj) = p(2);
+        As(ii,jj) = p(3);
+        R2(ii,jj) = rsquare(rS, p(1)*pdfcCu + (p(2)+p(1))*pdfcGa + (p(3)+p(2)+p(1))*pdfcAs);
     end
 end
 
