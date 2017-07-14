d77e9473c0b6fb0e99a27fec7ebc62977225d28a
Veerendra C Angadi <vcangadi1@users.noreply.github.com>
2017-07-14 19:42:18 +0100

    code cleaning for mlls_fit.m

--- mlls_fit.m
+++ mlls_fit.m
@@ -16,48 +16,6 @@ l = energy_loss_HIGH_LOSS;
 % High loss spectrum
 S = core_loss_spectrum;
 
-%% input variables
-
-% pixel location
-%ii = 28;
-%jj = 10;
-
-% load core-loss spectrum and energy-loss
-%load('C:\Users\elp13va.VIE\Desktop\EELS data\Ge-basedSolarCell_24082015\artifact_removed_EELS Spectrum Image disp1offset950time2s.mat');
-%load('/Users/veersaysit/Desktop/EELS data/Ge-basedSolarCell_24082015/artifact_removed_EELS Spectrum Image disp1offset950time2s.mat');
-%l = EELS.energy_loss_axis';
-%S = squeeze(EELS.SImage(ii,jj,:));
-%S = feval(Spline(l,S),l);
-
-% Load low-loss spectrum and energy-loss
-%EELZ = load('C:\Users\elp13va.VIE\Desktop\EELS data\Ge-basedSolarCell_24082015\artifact_removed_EELS Spectrum Image disp0.2offset0time0.1s.mat');
-%EELZ = load('/Users/veersaysit/Desktop/EELS data/Ge-basedSolarCell_24082015/artifact_removed_EELS Spectrum Image disp0.2offset0time0.1s.mat');
-%EELZ = EELZ.EELS;
-
-% Resample low-loss data
-%ll = resample(squeeze(EELZ.SImage(ii,jj,:)),1,round(EELS.dispersion/EELZ.dispersion));
-
-% Atomic Num
-%Atomic_Num = [29,31,33];
-
-% Energy onset in eV
-%Energy_onset_eV = [923,1115,1323];
-
-% Beam energy in kV
-%E0 = 197;
-
-% Collection angle in mrad
-%beta = 16.6;
-
-% Load differential cross sections
-%tic;
-%dfcCu = diffCS_L23(Atomic_Num(1),Energy_onset_eV(1),E0,beta,l);
-%dfcGa = diffCS_L23(Atomic_Num(2),Energy_onset_eV(2),E0,beta,l);
-%dfcAs = diffCS_L23(Atomic_Num(3),Energy_onset_eV(3),E0,beta,l);
-%dfc = [dfcCu, dfcGa, dfcAs];
-%toc;
-
-
 %% Get Residue of the spectrum
 % model background with exponential function
 b = model_begin_channel;
