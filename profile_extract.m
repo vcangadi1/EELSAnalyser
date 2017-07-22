2f2644c2f98182583e7f248770662a26c01375b2
Veerendra C Angadi <vcangadi1@users.noreply.github.com>
2017-07-22 21:03:44 +0100

    no modification in profile_extract.m

--- profile_extract.m
+++ profile_extract.m
@@ -3,7 +3,7 @@ function prof = profile_extract(I,Rotation_Angle)
 RI = imrotate(I,Rotation_Angle);
 
 Pro_X = ones(1,size(RI,1));
-for i = 1:size(RI,1),
+for i = 1:size(RI,1)
     Pro_X(i) = sum(squeeze(RI(i,:))>0);
 end
 Pro_X(Pro_X<=0)=1;
