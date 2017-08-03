0e4414d852339e00f68df517a3e8ace542f6f8dd
Veerendra C Angadi <vcangadi1@users.noreply.github.com>
2017-08-03 16:27:52 +0100

    made no changes to code

--- load_InGaN_SI6b_for_mlls.m
+++ load_InGaN_SI6b_for_mlls.m
@@ -5,7 +5,7 @@ EELS = readEELSdata('/Users/veersaysit/Desktop/EELS data/InGaN/100kV/EELS Spectr
 %EELS = change_dimension(EELS,90,43);
 Energy_shift = 7;
 EELS.energy_loss_axis = EELS.energy_loss_axis + Energy_shift;
-l = EELS.energy_loss_axis + Energy_shift;
+l = EELS.energy_loss_axis;
 
 EELZ = readEELSdata('/Users/veersaysit/Desktop/EELS data/InGaN/100kV/EELS Spectrum Image6-thickness-map.dm3');
 %EELZ = change_dimension(EELZ,90,43);
@@ -16,7 +16,7 @@ Optional_EELZ_low_loss = EELZ;
 bstar = 45;
 %[f1, f2, bstar] = Concor2(16.6,15,l,197);
 
-dfcIn = diffCS_M45(49,443,100,bstar,l);
+dfcIn = diffCS_M45(49,451,100,bstar,l);
 dfcGa = diffCS_L23(31,1115,100,bstar,l);
 dfcN = diffCS_K(7,400,100,bstar,l);
 Diff_cross_sections = [dfcN, dfcIn, dfcGa];
